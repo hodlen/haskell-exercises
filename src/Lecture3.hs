fd37718ad699f70a0836fd376d725ce88b825783
Holden <hodlenx@gmail.com>
2024-07-28 20:26:53 +0800

    finished lecture 3

--- src/Lecture3.hs
+++ src/Lecture3.hs
@@ -1,98 +1,98 @@
 {-# LANGUAGE InstanceSigs #-}
 
-{- |
-Module                  : Lecture3
-Copyright               : (c) 2021-2022 Haskell Beginners 2022 Course
-SPDX-License-Identifier : MPL-2.0
-Maintainer              : Haskell Beginners 2022 Course <haskell.beginners2022@gmail.com>
-Stability               : Stable
-Portability             : Portable
-
-Exercises for the Lecture 3 of the Haskell Beginners course.
-
-In this module you're going to practice standard Haskell typeclasses:
-
-  * Deriving instances
-  * Using typeclasses methods
-  * Implementing instances manually
-  * Becoming friends with Semigroup, Monoid, Foldable and Functor typeclasses!
-
--}
-
+-- |
+-- Module                  : Lecture3
+-- Copyright               : (c) 2021-2022 Haskell Beginners 2022 Course
+-- SPDX-License-Identifier : MPL-2.0
+-- Maintainer              : Haskell Beginners 2022 Course <haskell.beginners2022@gmail.com>
+-- Stability               : Stable
+-- Portability             : Portable
+--
+-- Exercises for the Lecture 3 of the Haskell Beginners course.
+--
+-- In this module you're going to practice standard Haskell typeclasses:
+--
+--   * Deriving instances
+--   * Using typeclasses methods
+--   * Implementing instances manually
+--   * Becoming friends with Semigroup, Monoid, Foldable and Functor typeclasses!
 module Lecture3
-    ( Weekday (..)
-    , toShortString
-    , next
-    , daysTo
-
-    , Gold (..)
-    , Reward (..)
-    , List1 (..)
-    , Treasure (..)
-
-    , appendDiff3
-    , apply
-    ) where
+  ( Weekday (..),
+    toShortString,
+    next,
+    daysTo,
+    Gold (..),
+    Reward (..),
+    List1 (..),
+    Treasure (..),
+    appendDiff3,
+    apply,
+  )
+where
 
 -- VVV If you need to import libraries, do it after this line ... VVV
 
--- ^^^ and before this line. Otherwise the test suite might fail  ^^^
+-- ^ ^^ and before this line. Otherwise the test suite might fail  ^^^
 
 -- $setup
 -- >>> import Data.Semigroup
 
-{- | Let's define a simple enumeration data type for representing days
-of the week.
--}
+-- | Let's define a simple enumeration data type for representing days
+-- of the week.
 data Weekday
-    = Monday
-    | Tuesday
-    | Wednesday
-    | Thursday
-    | Friday
-    | Saturday
-    | Sunday
-    deriving (Show, Eq)
+  = Monday
+  | Tuesday
+  | Wednesday
+  | Thursday
+  | Friday
+  | Saturday
+  | Sunday
+  deriving (Show, Eq, Bounded, Enum)
 
-{- | Write a function that will display only the first three letters
-of a weekday.
+-- | Write a function that will display only the first three letters
+-- of a weekday.
+--
+-- >>> toShortString Monday
+-- "Mon"
+toShortString :: Weekday -> String
+toShortString weekdy = take 3 (show weekdy)
 
->>> toShortString Monday
-"Mon"
--}
-toShortString = error "TODO"
+-- | Write a function that returns next day of the week, following the
+-- given day.
+--
+-- >>> next Monday
+-- Tuesday
+--
+-- ♫ NOTE: Implement this function without pattern matching on every
+--  constructor! Use standard typeclasses instead (you may need to derive
+--  them first).
+--
+-- 🕯 HINT: Check 'Enum' and 'Bounded' typeclasses.
+--
+-- 🆙 Bonus challenge 1: Could you implement this function in a such way
+--  that it'll still work even if you change constructor names and their
+--  order in the 'Weekday' type?
+--
+-- 🆙 Bonus challenge 2: Now, could you improve the implementation so it
+--  would work for **any** enumeration type in Haskell (e.g. 'Bool',
+--  'Ordering') and not just 'Weekday'?
+next :: (Enum a, Bounded a, Eq a) => a -> a
+next x
+    | x == maxBound = minBound
+    | otherwise = succ x
 
-{- | Write a function that returns next day of the week, following the
-given day.
 
->>> next Monday
-Tuesday
-
-♫ NOTE: Implement this function without pattern matching on every
-  constructor! Use standard typeclasses instead (you may need to derive
-  them first).
-
-🕯 HINT: Check 'Enum' and 'Bounded' typeclasses.
-
-🆙 Bonus challenge 1: Could you implement this function in a such way
-  that it'll still work even if you change constructor names and their
-  order in the 'Weekday' type?
-
-🆙 Bonus challenge 2: Now, could you improve the implementation so it
-  would work for **any** enumeration type in Haskell (e.g. 'Bool',
-  'Ordering') and not just 'Weekday'?
--}
-next = error "TODO"
-
-{- | Implement a function that calculates number of days from the first
-weekday to the second.
-
->>> daysTo Monday Tuesday
-1
->>> daysTo Friday Wednesday
-5
--}
-daysTo = error "TODO"
+-- | Implement a function that calculates number of days from the first
+-- weekday to the second.
+--
+-- >>> daysTo Monday Tuesday
+-- 1
+-- >>> daysTo Friday Wednesday
+-- 5
+daysTo :: Weekday -> Weekday -> Int
+daysTo fromDay toDay
+    | fromDay == toDay = 0
+    | otherwise = 1 + daysTo (next fromDay) toDay
 
 {-
 
@@ -103,85 +103,89 @@ have a lawful 'Monoid' instance.
 -}
 
 newtype Gold = Gold
-    { unGold :: Int
-    } deriving (Show, Eq)
+  { unGold :: Int
+  }
+  deriving (Show, Eq)
 
 -- | Addition of gold coins.
 instance Semigroup Gold where
-
+    (<>) (Gold a) (Gold b) = Gold (a + b)
 
 instance Monoid Gold where
+    mempty = Gold 0
 
-
-{- | A reward for completing a difficult quest says how much gold
-you'll receive and whether you'll get a special reward.
-
-If you combine multiple rewards, the final reward will contain a
-special prize if at least one of the rewards is special.
--}
+-- | A reward for completing a difficult quest says how much gold
+-- you'll receive and whether you'll get a special reward.
+--
+-- If you combine multiple rewards, the final reward will contain a
+-- special prize if at least one of the rewards is special.
 data Reward = Reward
-    { rewardGold    :: Gold
-    , rewardSpecial :: Bool
-    } deriving (Show, Eq)
+  { rewardGold :: Gold,
+    rewardSpecial :: Bool
+  }
+  deriving (Show, Eq)
 
 instance Semigroup Reward where
-
+    (<>) ra rb = Reward { rewardGold = rewardGold ra <> rewardGold rb, rewardSpecial = rewardSpecial ra || rewardSpecial rb }
 
 instance Monoid Reward where
+    mempty = Reward {rewardGold = mempty, rewardSpecial = False}
 
-
-{- | 'List1' is a list that contains at least one element.
--}
+-- | 'List1' is a list that contains at least one element.
 data List1 a = List1 a [a]
-    deriving (Show, Eq)
+  deriving (Show, Eq)
 
 -- | This should be list append.
 instance Semigroup (List1 a) where
+    (<>) (List1 x1 x1s) (List1 x2 x2s) = List1 x1 (x1s ++ [x2] ++ x2s)
 
+-- | Does 'List1' have the 'Monoid' instance? If no then why?
+--
+-- instance Monoid (List1 a) where
 
-{- | Does 'List1' have the 'Monoid' instance? If no then why?
-
-instance Monoid (List1 a) where
--}
-
-{- | When fighting a monster, you can either receive some treasure or
-don't.
--}
+-- | When fighting a monster, you can either receive some treasure or
+-- don't.
 data Treasure a
-    = NoTreasure
-    | SomeTreasure a
-    deriving (Show, Eq)
+  = NoTreasure
+  | SomeTreasure a
+  deriving (Show, Eq)
 
-{- | When you append multiple treasures for fighting multiple
-monsters, you should get a combined treasure and not just the first
-(or last one).
+-- | When you append multiple treasures for fighting multiple
+-- monsters, you should get a combined treasure and not just the first
+-- (or last one).
+--
+-- 🕯 HINT: You may need to add additional constraints to this instance
+--  declaration.
+instance (Semigroup a) => Semigroup (Treasure a) where
+    (<>) NoTreasure NoTreasure = NoTreasure
+    (<>) NoTreasure (SomeTreasure t) = SomeTreasure t
+    (<>) (SomeTreasure t) NoTreasure = SomeTreasure t
+    (<>) (SomeTreasure tl) (SomeTreasure tr) = SomeTreasure (tl <> tr)
 
-🕯 HINT: You may need to add additional constraints to this instance
-  declaration.
--}
-instance Semigroup (Treasure a) where
+instance (Semigroup a) => Monoid (Treasure a) where
+    mempty = NoTreasure
 
-
-instance Monoid (Treasure a) where
-
-
-{- | Abstractions are less helpful if we can't write functions that
-use them!
-
-Implement a polymorphic function that takes three elements and appends
-together only different elements.
-
->>> appendDiff3 [1] [3, 2] [0, 5]
-[1,3,2,0,5]
->>> appendDiff3 [4] [2, 2] [2, 2]
-[4,2,2]
->>> appendDiff3 [1 .. 5] [1 .. 5] [1 .. 5]
-[1,2,3,4,5]
->>> appendDiff3 (Product 2) (Product 3) (Product 3)
-Product {getProduct = 6}
-
--}
-appendDiff3 = error "TODO"
+-- | Abstractions are less helpful if we can't write functions that
+-- use them!
+--
+-- Implement a polymorphic function that takes three elements and appends
+-- together only different elements.
+--
+-- >>> appendDiff3 [1] [3, 2] [0, 5]
+-- [1,3,2,0,5]
+-- >>> appendDiff3 [4] [2, 2] [2, 2]
+-- [4,2,2]
+-- >>> appendDiff3 [1 .. 5] [1 .. 5] [1 .. 5]
+-- [1,2,3,4,5]
+-- >>> appendDiff3 (Product 2) (Product 3) (Product 3)
+-- Product {getProduct = 6}
+appendDiff3 :: (Eq a, Semigroup a) => a -> a -> a -> a
+appendDiff3 a b c
+    | a /= b && b /= c && a/= c = a <> b <> c
+    | a /= b = a <> b
+    | a /= c = a <> c
+    | b /= c = b <> c
+    | otherwise = a
 
 {-
 
@@ -213,8 +217,15 @@ types that can have such an instance.
 -- instance Foldable Weekday where
 -- instance Foldable Gold where
 -- instance Foldable Reward where
--- instance Foldable List1 where
--- instance Foldable Treasure where
+instance Foldable List1 where
+    foldr step zero (List1 x []) = step x zero
+    foldr step zero (List1 x0 (x1:xs)) = step x0 (foldr step zero (List1 x1 xs))
+
+instance Foldable Treasure where
+    foldr _ zero NoTreasure = zero
+    foldr step zero (SomeTreasure t) = step t zero
+    foldMap _ NoTreasure = mempty
+    foldMap step (SomeTreasure t) = step t
 
 {-
 
@@ -229,24 +240,27 @@ types that can have such an instance.
 -- instance Functor Weekday where
 -- instance Functor Gold where
 -- instance Functor Reward where
--- instance Functor List1 where
--- instance Functor Treasure where
+instance Functor List1 where
+    fmap f (List1 x []) = List1 (f x) []
+    fmap f (List1 x xs) = List1 (f x) (fmap f xs)
+instance Functor Treasure where
+    fmap _ NoTreasure = NoTreasure
+    fmap f (SomeTreasure t) = SomeTreasure (f t)
 
-{- | Functions are first-class values in Haskell. This means that they
-can be even stored inside other data types as well!
-
-Now, you have a function inside some 'Functor'. You're a given an
-element and you need to apply the function inside the 'Functor' to a
-given element.
-
->>> apply 5 (Just (+ 3))
-Just 8
->>> apply 5 Nothing
-Nothing
->>> apply [1 .. 10] (Just (drop 7))
-Just [8,9,10]
->>> apply 5 [(+ 3), (* 4), div 17]
-[8,20,3]
-
--}
-apply = error "TODO"
+-- | Functions are first-class values in Haskell. This means that they
+-- can be even stored inside other data types as well!
+--
+-- Now, you have a function inside some 'Functor'. You're a given an
+-- element and you need to apply the function inside the 'Functor' to a
+-- given element.
+--
+-- >>> apply 5 (Just (+ 3))
+-- Just 8
+-- >>> apply 5 Nothing
+-- Nothing
+-- >>> apply [1 .. 10] (Just (drop 7))
+-- Just [8,9,10]
+-- >>> apply 5 [(+ 3), (* 4), div 17]
+-- [8,20,3]
+apply :: Functor f => a -> f (a -> b) -> f b
+apply x = fmap (\f -> f x)
